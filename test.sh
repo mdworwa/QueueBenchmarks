efcf7063f3afa22102123fae40fa747e26015da1
Chameleon Cloud User <cc@benchmarkh.novalocal>
2018-07-02 22:49:49 +0000

    SQueue tests

--- test.sh
+++ test.sh
@@ -1,12 +1,13 @@
-types=(1 2 3)
+types=(1)
 num_threads=(2 4 6 8 12 16 24 32 48 64)
+TEST="LatencyTests"
 
 for type in "${types[@]}"
 do
 	for threads in "${num_threads[@]}"
 	do
 		echo "Benchmarking $type with $threads thread(s)"
-		./main ${type} ${threads}
+		./main ${type} ${threads} ${TEST}
 		echo ""
 	done
 done 
